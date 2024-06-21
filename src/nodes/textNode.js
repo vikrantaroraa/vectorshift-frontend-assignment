6825f776bd73c17d69d4e4d98f47729fc2d86369
vikrantaroraa <arora.veshu@gmail.com>
2024-06-21 16:11:01 +0530

    feat: added the feature to auto grow the node as text is entered into the textNode

--- src/nodes/textNode.js
+++ src/nodes/textNode.js
@@ -1,14 +1,31 @@
 // textNode.js
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Handle, Position } from "reactflow";
 import styles from "./textNode.module.css";
 
 export const TextNode = ({ id, data }) => {
   const [currText, setCurrText] = useState(data?.text || "{{input}}");
+  const textareaRef = useRef();
 
   const handleTextChange = (e) => {
     setCurrText(e.target.value);
+  };
+
+  useEffect(() => {
+    const textareaElement = textareaRef.current;
+    console.log(textareaElement);
+    textareaElement.setAttribute(
+      "style",
+      "height:" + textareaElement.scrollHeight + "px;overflow-y:hidden;"
+    );
+    textareaElement.addEventListener("input", autoGrow, false);
+  }, []);
+
+  const autoGrow = () => {
+    const textareaElement = textareaRef.current;
+    textareaElement.style.height = "auto";
+    textareaElement.style.height = textareaElement.scrollHeight + "px";
   };
 
   return (
@@ -19,7 +36,13 @@ export const TextNode = ({ id, data }) => {
       <div className={styles["node-body"]}>
         <label className={styles["node-data"]}>
           <span>Text</span>
-          <input type="text" value={currText} onChange={handleTextChange} />
+          <textarea
+            type="text"
+            value={currText}
+            onChange={handleTextChange}
+            ref={textareaRef}
+            className="nodrag"
+          />
         </label>
       </div>
       <Handle
